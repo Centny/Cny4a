d2e68a6c4527f12fa591e3b187c485a529384330
Centny <centny@gmail.com>
2013-12-27 22:03:33 +0800

    update build script.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
+cp -r test/* src
 export PATH=$PATH:$GOROOT/bin:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANT_HOME/bin
 ant stop_ts start_ts uninstall emma debug install test fetch-test-report stop_ts
